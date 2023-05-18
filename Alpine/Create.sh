d1e2a0d341c1a41f90aa0d6529c276e2a64d9a7f
AKEBI-NUKUI <132581172+AKEBI-NUKUI@users.noreply.github.com>
2023-05-18 17:00:29 +0800

    Create Create.sh

---
+++ Alpine/Create.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# 生成随机密码
+GEN_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+echo "随机密码：${GEN_PASS}"
+
+# 读取 SSH 端口和转发端口
+read -e -p "请输入 SSH 端口: " ssh_port
+read -e -p "请输入开始转发端口: " tran_port_start
+read -e -p "请输入结束转发端口: " tran_port_end
+read -e -p "CPU峰值,已转换为百分值: " cpu
+read -e -p "分配内存值: " ram
+
+# 替换 Dockerfile 中的密码
+sed "s/{{ROOT_PASSWORD}}/$GEN_PASS/" Dockerfile > Dockerfile.tmp
+
+# 构建 Docker 镜像
+docker build -t alpine$ssh_port -f Dockerfile.tmp .
+
+# 启动 Docker 容器
+docker run --name alpine$ssh_port -p $ssh_port:22 -p $tran_port_start-$tran_port_end:$tran_port_start-$tran_port_end -d --restart always --cpus 0.$cpu --memory "$ram"m alpine$ssh_port
+
+# 删除临时文件
+rm -f Dockerfile.tmp
+
+echo "SSH端口: $ssh_port 密码: $GEN_PASS 端口: $tran_port_start - $tran_port_end OS:Alpine" >> output.log

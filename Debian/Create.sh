daae9febd7f27035370b17e44a7501003610919c
AKEBI-NUKUI <132581172+AKEBI-NUKUI@users.noreply.github.com>
2023-05-18 17:08:25 +0800

    Update Create.sh

--- Debian/Create.sh
+++ Debian/Create.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
+ssh_port=$1
+tran_port_start=$2
+tran_port_end=$3
+cpu=$4
+ram=$5
+
 # 生成随机密码
 GEN_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 echo "随机密码：${GEN_PASS}"
 
-# 读取 SSH 端口和转发端口
-read -e -p "请输入 SSH 端口: " ssh_port
-read -e -p "请输入开始转发端口: " tran_port_start
-read -e -p "请输入结束转发端口: " tran_port_end
-read -e -p "CPU峰值,已转换为百分值: " cpu
-read -e -p "分配内存值: " ram
+# 读取 配置
+if [ -z "$ssh_port" ]; then
+  read -e -p "请输入小鸡SSH端口: " ssh_port
+fi
+if [ -z "$tran_port_start" ]; then
+  read -e -p "请输入开始转发端口: " tran_port_start
+fi
+if [ -z "$tran_port_end" ]; then
+  read -e -p "请输入结束转发端口: " tran_port_end
+fi
+if [ -z "$cpu" ]; then
+  read -e -p "CPU峰值,已转换为百分值，直接输入50，则为50%: " cpu
+fi
+if [ -z "$ram" ]; then
+  read -e -p "分配内存MB: " ram
+fi
 
 # 替换 Dockerfile 中的密码
 sed "s/{{ROOT_PASSWORD}}/$GEN_PASS/" Dockerfile > Dockerfile.tmp
